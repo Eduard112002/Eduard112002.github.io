eba301ee4b69fc8dd68cea73a01f776df4329e80
Эдуард <123071598+Eduard112002@users.noreply.github.com>
2023-04-21 00:32:09 +0300

    Delete img directory

--- img/Mak_1.5.ts
+++
@@ -1,8 +0,0 @@
-
-function further() {
-    let slider_inner = document.querySelectorAll(".slider-slides > button");
-    let but = document.querySelector('.further');
-
-    slider_inner.classList.remove('tablet');
-    but.textContent = 'Скрыть';
-}
